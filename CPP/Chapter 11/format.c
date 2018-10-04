6d8acb87d3e5b26077eb9740f3c65cd7da197989
Tom007Cheung <10154508169@stu.ecnu.edu.cn>
2018-10-04 11:44:18 +0800

    add sprintf() and sort strings function

---
+++ CPP/Chapter 11/format.c
@@ -0,0 +1,23 @@
+/* format.c -- format a string */
+#include <stdio.h>
+#include "./String_Functions/s_gets.h"
+#define MAX 20
+
+int main(void)
+{
+    char first[MAX];
+    char last[MAX];
+    char formal[2 * MAX + 10];
+    double prize;
+
+    puts("Enter your first name:");
+    s_gets(first, MAX);
+    puts("Enter your last name:");
+    s_gets(last, MAX);
+    puts("Enter your prize money:");
+    scanf("%lf", &prize);
+    sprintf(formal, "%s, %-19s: $%6.2f\n", last, first, prize);
+    puts(formal);
+
+    return 0;
+}
