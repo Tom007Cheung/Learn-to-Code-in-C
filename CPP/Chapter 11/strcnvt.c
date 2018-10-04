f098a13bd41cb064d9ba4b74e0c5917d7d54c2f1
Tom007Cheung <10154508169@stu.ecnu.edu.cn>
2018-10-04 15:12:17 +0800

    complete Chapter 11: Character Strings and String Functions

---
+++ CPP/Chapter 11/strcnvt.c
@@ -0,0 +1,26 @@
+/* strcnvt.c -- try strtol() */
+#include <stdio.h>
+#include <stdlib.h>
+#include "String_Functions/s_gets.h"
+#define LIM 30
+
+int main()
+{
+    char number[LIM];
+    char * end;
+    long value;
+
+    puts("Enter a number (empty line to quit):");
+    while (s_gets(number, LIM) && number[0] != '\0')
+    {
+        value = strtol(number, &end, 10); /* base 10 */
+        printf("base 10 input, base 10 output: %ld, stopped at %s (%d)\n",
+                value, end, *end);
+        value = strtol(number, &end, 16); /* base 16 */
+        printf("base 16 input, base 10 output: %ld, stopped at %s (%d)\n",
+                value, end, *end);
+        puts("Next number:");
+    }
+    puts("Bye\n");
+    return 0;
+}
